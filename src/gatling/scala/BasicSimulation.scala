39e4a13a2552b3dc5aff9bbb35dcc70ea55c61f8
hongkailiu <hongkailiu@users.noreply.github.com>
2016-04-24 21:12:48 -0400

    start gatling config

--- src/gatling/scala/BasicSimulation.scala
+++ src/gatling/scala/BasicSimulation.scala
@@ -12,7 +12,7 @@ class BasicSimulation extends Simulation {
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
   val scn = scenario("BasicSimulation") // 7
-    .exec(http("request_1")  // 8
+    .exec(http("/stat/general")  // 8
     .get("/stat/general")) // 9
     .pause(5) // 10
 
